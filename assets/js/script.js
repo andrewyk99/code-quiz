a734b27326ca19a871fe93a26e203b7d48ca2cd8
andrewyk99 <andrewyk99@hotmail.com>
2022-03-08 16:08:09 -0700

    added bulk of css styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,33 +10,33 @@ var loadedHighScores = [];
 
 // questions
 var question0 = {
-    question: "Test Question 1",
-    answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-    corrAnswer: "Answer 2"
+    question: "What does HTML stand for",
+    answers: ["Hyper Text Preprocessor", "Hyper Text Markup Language", "Hyper Text Multiple Language", "Hyper Tool Multi Language"],
+    corrAnswer: "Hyper Text Markup Language"
 };
 
 var question1 = {
-    question: "Test Question 2",
-    answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-    corrAnswer: "Answer 3"
+    question: "What does CSS stand for?",
+    answers: ["Common Style Sheet", "Colorful Style Sheet", "Computer Style Sheet", "Cascading Style Sheet"],
+    corrAnswer: "Cascading Style Sheet"
 };
 
 var question2 = {
-    question: "Test Question 3",
-    answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-    corrAnswer: "Answer 1"
+    question: "The condition in an if / else statement is enclosed with:",
+    answers: ["Quotes", "Curly Brackets", "Parenthesis", "Square Brackets"],
+    corrAnswer: "Parenthesis"
 };
 
 var question3 = {
-    question: "Test Question 4",
-    answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-    corrAnswer: "Answer 4"
+    question: "Arrays in JavaScript can be used to store:",
+    answers: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the above"],
+    corrAnswer: "All of the above"
 };
 
 var question4 = {
-    question: "Test Question 5",
-    answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-    corrAnswer: "Answer 2"
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answers: ["JavaScript", "Terminal/Bash", "For Loops", "console.log"],
+    corrAnswer: "console.log"
 };
 
 var questionList = [question0, question1, question2, question3, question4];
@@ -45,36 +45,41 @@ var questionNumb = 0;
 
 // intro
 var intro = function() {
+    // resets functions for replayibility
     borderEl.innerHTML = "";
     timer = 0;
     timerEl.innerHTML = "Timer: " + timer;
     questionNumb = 0;
+    
+    // creates subtitle
     var titleEl = document.createElement("div");
     titleEl.textContent = "Coding Quiz Challenge";
     titleEl.className = "title";
     borderEl.appendChild(titleEl);
 
+    // creates instructions
     var instructionsEl = document.createElement("div");
     instructionsEl.innerHTML = "Try to answer the following code-related questions within the time limit.<br> Keep in mind that incorrect answers will penalize your score/time by ten seconds!";
     instructionsEl.className = "instructions";
     borderEl.appendChild(instructionsEl);
 
+    // creates start button to begin the game
     var startBtnEl = document.createElement("button");
     startBtnEl.textContent = "Start Quiz!";
-    startBtnEl.className = "start-btn";
+    startBtnEl.className = "start-btn btn";
     borderEl.appendChild(startBtnEl);
 };
 
 // startBtn is clicked
 var startBtn = function() {
-    timer = 60;
+    timer = 50; // time set to 50 so that player can't get any points if all answers are answered incorrectly
     timerEl.innerHTML = "Timer: " + timer;
     var timerInterval = setInterval(function() {
-        if (timer < 0) {
+        if (timer < 0) { // prevents time/score to become negative
             timer = 0
             resultPage();
         }
-        if (timer === 0 || timerFlag === 1) {
+        if (timer === 0 || timerFlag === 1) { // if timer goes to 0, ends the game and skips remaining questions to result page
             clearInterval(timerInterval);
         }
         if (timer > 0) {
@@ -83,22 +88,29 @@ var startBtn = function() {
         timerEl.innerHTML = "Timer: " + timer;
     }, 1000);
 
+    // runs questions() function
     questions();
 };
 
 // lists questions and answer choices
 var questions = function() {
     borderEl.innerHTML = "";
-    borderEl.textContent = questionList[questionNumb].question;
+    var questionEl = document.createElement("div");
+    questionEl.className = "title";
+    borderEl.appendChild(questionEl);
+    questionEl.textContent = questionList[questionNumb].question;
 
     var answerChoicesEl = document.createElement("ol");
+    answerChoicesEl.className = "answer-list"
     borderEl.appendChild(answerChoicesEl);
     for (var i = 0; i < 4; i++) {
         var answerChoiceEl = document.createElement("li");
         answerChoiceEl.className = "answer-choices";
-        answerChoiceEl.textContent = question0.answers[i];
+        answerChoiceEl.textContent = questionList[questionNumb].answers[i];
         answerChoicesEl.appendChild(answerChoiceEl);
     }
+
+    questionList[questionNumb].question.className = "title";
 
     var resultEl = document.createElement("div");
 }
@@ -138,10 +150,12 @@ var resultPage = function() {
     }
     var finalScoreEl = document.createElement("div");
     finalScoreEl.textContent = "Your final score is " + score + "!";
+    finalScoreEl.className = "info"
     borderEl.appendChild(finalScoreEl);
 
     var initialsEl = document.createElement("form");
-    initialsEl.innerHTML = "Enter Initial: <input type='text' name='score-name'/> <button class='submit' type='submit'>Submit</button>";
+    initialsEl.innerHTML = "Enter Initial: <input type='text' name='score-name'/> <button class='submit btn' type='submit'>Submit</button>";
+    initialsEl.className = "enterInitial"
     borderEl.appendChild(initialsEl);
 };
 
@@ -184,22 +198,24 @@ var highScore = function() {
     highScoreTitle.className = "title";
     borderEl.appendChild(highScoreTitle);
     
-    var highScoreList = document.createElement("ol");
-    borderEl.appendChild(highScoreList);
+    var highScoreListEl = document.createElement("ol");
+    highScoreListEl.className = "leaderboard-table"
+    borderEl.appendChild(highScoreListEl);
     for (var i = 0; i < 5; i++) {
-        var topHighScores = document.createElement("li");
-        highScoreList.appendChild(topHighScores);
-        topHighScores.textContent = loadedHighScores[i].initial + " - " + loadedHighScores[i].score;
+        var topHighScoresEl = document.createElement("li");
+        topHighScoresEl.className = "leaderboard";
+        highScoreListEl.appendChild(topHighScoresEl);
+        topHighScoresEl.textContent = loadedHighScores[i].initial + " - " + loadedHighScores[i].score;
     }
 
     var goBack = document.createElement("button");
     goBack.textContent = "Go Back";
-    goBack.className = "btn-back";
+    goBack.className = "btn-back btn";
     borderEl.appendChild(goBack);
 
     var clearScore = document.createElement("button");
     clearScore.textContent = "Clear Score";
-    clearScore.className = "btn-clear";
+    clearScore.className = "btn-clear btn";
     borderEl.appendChild(clearScore);
 };
 
